5499b41c32b4d1efb396ef7a3dc157335db2eefe
didierthomasm <didierthomas.m@gmail.com>
2023-07-13 20:51:50 -0600

    changes in the html and js, and creation of the readme

--- README.md
+++ README.md
@@ -1,1 +1,76 @@
-# Work-Day-Scheduler
+# Work Day Scheduler
+
+A simple calendar application that allows users to schedule events for each hour of a typical working day (9am–5pm). The application features dynamically updated HTML and CSS powered by jQuery and utilizes the Day.js library to work with date and time.
+
+## Table of Contents
+
+- [Features](#features)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Technologies Used](#Technologies-Used)
+- [Folder Structure](#folder-structure)
+- [Credits](#credits)
+- [Contributing](#contributing)
+- [License](#license)
+- [Deployed](#deployed)
+
+## Features
+
+- When I open the planner, the current day is displayed at the top of the calendar.
+- When I scroll down, I am presented with time blocks for the standard business hours of 9am–5pm.
+- When I view the time blocks for that day, each time block is color-coded to indicate whether it is in the past, present, or future.
+- When I click into a time block, I can enter an event.
+- When I click the save button for that time block, the text for that event is saved in local storage.
+- When I refresh the page, the saved events persist.
+
+## Installation
+
+1. Clone the repository or download the source code.
+2. Open the index.html file in a web browser.
+
+## Usage
+
+1. Open the `index.html` file in a web browser.
+2. The current date will be displayed at the top of the calendar.
+3. Scroll down to view the time blocks for the day (9am–5pm).
+4. Each time block will be color-coded to indicate whether it is in the past (gray), present (red), or future (green).
+5. Click into a time block to enter an event.
+6. Click the save button (floppy disk icon) for that time block to save the event text in local storage.
+7. Refresh the page, and the saved events will persist.
+
+## Technologies Used
+
+- HTML
+- CSS (Bootstrap)
+- JavaScript (jQuery)
+- Day.js
+
+## Folder Structure
+
+- `index.html`: The main HTML file that displays the calendar application.
+- `assets/`: The folder containing CSS, JavaScript, and other assets.
+    - `css/`: Contains the CSS files.
+    - `js/`: Contains the JavaScript file for the calendar application.
+    - `style.css`: The CSS file that styles the calendar application.
+    - `script.js`: The JavaScript file that provides the functionality for the calendar application.
+
+## Credits
+
+- jQuery: [https://jquery.com/](https://jquery.com/)
+- Bootstrap: [https://getbootstrap.com/](https://getbootstrap.com/)
+- Font Awesome: [https://fontawesome.com/](https://fontawesome.com/)
+- Day.js: [https://day.js.org/](https://day.js.org/)
+
+## Contributing
+
+Contributions are welcome! If you encounter any issues or have suggestions for improvements,
+please feel free to submit a pull request or create an issue in the repository.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+## Deployed
+<https://didierthomasm.github.io/Work-Day-Scheduler/>
+<br>
+<img src="./assets/img/ScreenshotWorkDayScheduler.png" width="500px">
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,67 +1,88 @@
-$(document).ready(function() {
-
+document.addEventListener("DOMContentLoaded", function() {
+  
+  let fragment = document.createDocumentFragment();
+  // Create 9 blocks with the corresponding hour labels
   for (let i = 0; i < 9; i++) {
+    // Create the parent div of the next elements
     let firstDiv = document.createElement('div');
     firstDiv.className = 'row time-block';
     firstDiv.id = `hour-${9 + i}`;
-    console.log(`hour-${9 + i}`);
+    // Create a div element and set the dataset.hour attribute
+    let secondDiv = document.createElement(('div'));
+    secondDiv.className = 'col-2 col-md-1 hour text-center py-3';
+    if (i < 3){
+      secondDiv.dataset.hour = `${9 + i}AM`;
+    }
+    else if (i === 3){
+      secondDiv.dataset.hour = `${9 + i}PM`;
+    }
+    else {
+      secondDiv.dataset.hour = `${i - 3}PM`;
+    }
+    // Create the text area
+    let textArea = document.createElement('textarea');
+    textArea.className = 'col-8 col-md-10 description';
+    textArea.rows = 3;
+    // Create a button element
+    let button = document.createElement('button');
+    button.className = 'btn saveBtn col-2 col-md-1';
+    button.ariaLabel = 'save';
+    // Create an icon element
+    let icon = document.createElement('i');
+    icon.className = 'fas fa-save';
+    icon.ariaHidden = 'true';
+    // Append the created elements
+    fragment.appendChild(firstDiv);
+    firstDiv.append(secondDiv);
+    firstDiv.append(textArea);
+    firstDiv.append(button);
+    button.append(icon);
   }
+  document.getElementById('container').appendChild(fragment);
 
-  /*    <div id="hour-9" className="row time-block">
-        <div className="col-2 col-md-1 hour text-center py-3" data-hour="9AM"></div>
-        <textarea className="col-8 col-md-10 description" rows="3"> </textarea>
-        <button className="btn saveBtn col-2 col-md-1" aria-label="save">
-          <i className="fas fa-save" aria-hidden="true"></i>
-        </button>
-      </div>*/
-  
   // Add a listener for click events on the save button
-  $(".saveBtn").on("click", function() {
+  $("#container").on("click", ".saveBtn", function() {
 
     // Get the description text from the textarea within the same time block
     let description = $(this).siblings(".description").val().trim();
 
     // Get the id of the containing time-block (e.g., "hour-9")
-    let timeBlockId = $(this).closest(".time-block").attr("id");
+    let timeBlockId = this.closest(".time-block").id;
+
 
     // Save the description in local storage using the time block id as the key
     localStorage.setItem(timeBlockId, description);
   });
   
   // Display the hour of the day next to the text area
-  $('.hour').each(function () {
-    let info = $(this).data('hour');
-    $(this).text(info);
+  let hourElements = document.querySelectorAll('.hour');
+  hourElements.forEach(function(element) {
+    element.textContent = element.dataset.hour;
   });
 
   // Apply the past, present, or future class to each time block
-  $(".time-block").each(function() {
-    let hour = parseInt($(this).attr("id").split("-")[1]);
+  let timeBlockElements = document.querySelectorAll(".time-block");
+  timeBlockElements.forEach(function(element) {
+    let hour = parseInt(element.id.split("-")[1]);
     let currentHour = dayjs().hour();
-    
-    $(this).removeClass('past present future')
-    
+    // Remove 'past present future' existing class
+    element.classList.remove('past', 'present', 'future');
+    // Depending on the hour set the class
     if (hour < currentHour) {
-      $(this).addClass("past");
+      element.classList.add("past");
     } else if (hour === currentHour) {
-      $(this).addClass("present");
+      element.classList.add("present");
     } else {
-      $(this).addClass("future");
+      element.classList.add("future");
     }
   });
 
   // Get user input from local storage and set the textarea values
-  $(".time-block").each(function() {
-    let timeBlockId = $(this).attr("id");
-    let description = localStorage.getItem(timeBlockId);
-    $(this).find(".description").val(description);
+  timeBlockElements.forEach(function(element) {
+    let timeBlockId = element.id;
+    element.querySelector(".description").value = localStorage.getItem(timeBlockId);
   });
 
   // Display the current date in the header of the page
-  $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"));
-  
-  // Refresh the page every hour
-  setInterval(function () {
-    location.reload();
-  }, 3600000);
+  document.getElementById("currentDay").textContent = dayjs().format("dddd, MMMM D, YYYY");
 });
