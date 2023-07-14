78bff34f877536ed74ac636704b806c5231b3bd5
didierthomasm <didierthomas.m@gmail.com>
2023-07-13 21:15:46 -0600

    last minute changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,51 @@
-document.addEventListener("DOMContentLoaded", function() {
-  
+$(document).ready(function() {
+
   let fragment = document.createDocumentFragment();
+
   // Create 9 blocks with the corresponding hour labels
   for (let i = 0; i < 9; i++) {
     // Create the parent div of the next elements
     let firstDiv = document.createElement('div');
     firstDiv.className = 'row time-block';
     firstDiv.id = `hour-${9 + i}`;
+
     // Create a div element and set the dataset.hour attribute
-    let secondDiv = document.createElement(('div'));
+    let secondDiv = document.createElement('div');
     secondDiv.className = 'col-2 col-md-1 hour text-center py-3';
-    if (i < 3){
+
+    if (i < 3) {
       secondDiv.dataset.hour = `${9 + i}AM`;
-    }
-    else if (i === 3){
+    } else if (i === 3) {
       secondDiv.dataset.hour = `${9 + i}PM`;
-    }
-    else {
+    } else {
       secondDiv.dataset.hour = `${i - 3}PM`;
     }
+
     // Create the text area
     let textArea = document.createElement('textarea');
     textArea.className = 'col-8 col-md-10 description';
     textArea.rows = 3;
+
     // Create a button element
     let button = document.createElement('button');
     button.className = 'btn saveBtn col-2 col-md-1';
-    button.ariaLabel = 'save';
+    button.setAttribute('aria-label', 'save');
+
     // Create an icon element
     let icon = document.createElement('i');
     icon.className = 'fas fa-save';
-    icon.ariaHidden = 'true';
+    icon.setAttribute('aria-hidden', 'true');
+
     // Append the created elements
     fragment.appendChild(firstDiv);
-    firstDiv.append(secondDiv);
-    firstDiv.append(textArea);
-    firstDiv.append(button);
-    button.append(icon);
+    firstDiv.appendChild(secondDiv);
+    firstDiv.appendChild(textArea);
+    firstDiv.appendChild(button);
+    button.appendChild(icon);
   }
-  document.getElementById('container').appendChild(fragment);
+  
+
+  $("#container").append(fragment);
 
   // Add a listener for click events on the save button
   $("#container").on("click", ".saveBtn", function() {
@@ -47,7 +54,7 @@ document.addEventListener("DOMContentLoaded", function() {
     let description = $(this).siblings(".description").val().trim();
 
     // Get the id of the containing time-block (e.g., "hour-9")
-    let timeBlockId = this.closest(".time-block").id;
+    let timeBlockId = $(this).closest(".time-block").attr("id");
 
 
     // Save the description in local storage using the time block id as the key
@@ -61,28 +68,28 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 
   // Apply the past, present, or future class to each time block
-  let timeBlockElements = document.querySelectorAll(".time-block");
-  timeBlockElements.forEach(function(element) {
-    let hour = parseInt(element.id.split("-")[1]);
+  $(".time-block").each(function() {
+    let hour = parseInt($(this).attr("id").split("-")[1]);
     let currentHour = dayjs().hour();
     // Remove 'past present future' existing class
-    element.classList.remove('past', 'present', 'future');
+    $(this).removeClass('past', 'present', 'future');
     // Depending on the hour set the class
     if (hour < currentHour) {
-      element.classList.add("past");
+      $(this).addClass("past");
     } else if (hour === currentHour) {
-      element.classList.add("present");
+      $(this).addClass("present");
     } else {
-      element.classList.add("future");
+      $(this).addClass("future");
     }
   });
 
   // Get user input from local storage and set the textarea values
-  timeBlockElements.forEach(function(element) {
-    let timeBlockId = element.id;
-    element.querySelector(".description").value = localStorage.getItem(timeBlockId);
+  $(".time-block").each(function() {
+    let timeBlockId = $(this).attr("id");
+    let description = localStorage.getItem(timeBlockId);
+    $(this).find(".description").val(description);
   });
 
   // Display the current date in the header of the page
-  document.getElementById("currentDay").textContent = dayjs().format("dddd, MMMM D, YYYY");
+  $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"));
 });
